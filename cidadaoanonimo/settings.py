27464132b57f0930265890086c34f7f3f8d733ca
Miguel Becker <habranea@disroot.org>
2026-06-12 16:26:01 -0300

    Update settings.py

--- cidadaoanonimo/settings.py
+++ cidadaoanonimo/settings.py
@@ -58,11 +58,8 @@ WSGI_APPLICATION = 'cidadaoanonimo.wsgi.application'
 # Database configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'root',
-        'PASSWORD': 'qwerasdf',
-        'HOST': '127.0.0.1',
-        'NAME': 'denunciadb'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
